fd61f2b96852c8562158f8c1caae8ff400556658
Michael Tiller <michael.tiller@gmail.com>
2015-01-10 18:53:51 -0500

    Fixing a serious bug in the Follow command
    The issue here is that the relations array was not getting properly extended.  The
    call to copy wasn't doing what was expected.  I've replaced that code with something
    that is probably less efficient, but easier to read and definitely works.  It
    basically builds a whole new array by appending the previous elements and then
    one additional element.
    
    I also created some tests to make sure that a double follow works.  This involved
    a couple of changes to the _links elements in the test case.  It didn't change any
    of the existing tests, just added a few new ones that leveraged the new link
    relations that I added.

--- navigator.go
+++ navigator.go
@@ -2,6 +2,7 @@ package halgo
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -99,8 +100,7 @@ func (n navigator) Follow(rel string) navigator {
 // Followf adds a relation to the follow queue of the navigator, with a
 // set of parameters to expand on execution.
 func (n navigator) Followf(rel string, params P) navigator {
-	relations := make([]relation, 0, len(n.path)+1)
-	copy(n.path, relations)
+	relations := append([]relation{}, n.path...)
 	relations = append(relations, relation{rel: rel, params: params})
 
 	return navigator{
@@ -118,7 +118,7 @@ func (n navigator) url() (string, error) {
 	for _, link := range n.path {
 		links, err := n.getLinks(url)
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("Error getting links (%s, %v): %v", url, links, err)
 		}
 
 		if _, ok := links.Items[link.rel]; !ok {
@@ -127,7 +127,7 @@ func (n navigator) url() (string, error) {
 
 		url, err = links.HrefParams(link.rel, link.params)
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("Error getting url (%v, %v): %v", link.rel, link.params, err)
 		}
 
 		if url == "" {
@@ -136,7 +136,7 @@ func (n navigator) url() (string, error) {
 
 		url, err = makeAbsoluteIfNecessary(url, n.rootUri)
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("Error making url absolute: %v", err)
 		}
 	}
 
@@ -300,7 +300,7 @@ func (n navigator) getLinks(uri string) (Links, error) {
 	var m Links
 
 	if err := json.Unmarshal(body, &m); err != nil {
-		return Links{}, err
+		return Links{}, fmt.Errorf("Unable to unmarshal '%s': %v", string(body), err)
 	}
 
 	return m, nil
--- navigator_test.go
+++ navigator_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -16,8 +17,10 @@ func createTestHttpServer() (*httptest.Server, map[string]int) {
 		hits["/"] += 1
 		fmt.Fprintf(w, `{
       "_links": {
+        "self": { "href": "/" },
         "next": { "href": "http://%s/2nd" },
         "relative": { "href": "/2nd" },
+        "child": { "href": "/child" },
         "one": { "href": "http://%s/a/{id}", "templated": true }
       }
     }`, r.Host, r.Host)
@@ -35,6 +38,11 @@ func createTestHttpServer() (*httptest.Server, map[string]int) {
 		w.WriteHeader(200)
 	})
 
+	r.HandleFunc("/child", func(w http.ResponseWriter, r *http.Request) {
+		hits["/child"] += 1
+		fmt.Fprintf(w, `{ "_links": { "parent": { "href": "/" } } }`)
+	})
+
 	return httptest.NewServer(r), hits
 }
 
@@ -47,7 +55,8 @@ func TestNavigatingToUnknownLink(t *testing.T) {
 		t.Fatal("Expected error to be raised for missing link")
 	}
 
-	if err.Error() != "Response didn't contain 'missing' link relation: available options were ['next' 'relative' 'one']" {
+	// I use HasPrefix because the order of the available relations isn't deterministic
+	if !strings.HasPrefix(err.Error(), "Response didn't contain 'missing' link relation:") {
 		t.Errorf("Unexpected error message: %s", err.Error())
 	}
 
@@ -76,6 +85,69 @@ func TestGettingTheRoot(t *testing.T) {
 
 	if hits["/"] != 1 {
 		t.Errorf("Expected 1 request to /, got %d", hits["/"])
+	}
+}
+
+func TestGettingTheRootSelf(t *testing.T) {
+	ts, hits := createTestHttpServer()
+	defer ts.Close()
+
+	nav := Navigator(ts.URL)
+	res, err := nav.Follow("self").Get()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected OK, got %d", res.StatusCode)
+	}
+
+	if res.Request.URL.String() != ts.URL+"/" {
+		t.Errorf("Expected url to be %s, got %s", ts.URL+"/", res.Request.URL)
+	}
+
+	if hits["/"] != 2 {
+		t.Errorf("Expected 2 requests to /, got %d", hits["/"])
+	}
+}
+
+func TestGettingTheRootViaChild(t *testing.T) {
+	ts, hits := createTestHttpServer()
+	defer ts.Close()
+
+	nav := Navigator(ts.URL)
+
+	child := nav.Follow("child")
+	curl, err := child.url()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !strings.HasSuffix(curl, "/child") {
+		t.Errorf("Expected URL for child relation to end with %s, but got %s", "/child", curl)
+	}
+
+	root := child.Follow("parent")
+	fmt.Printf("root = %v\n", root)
+	_, err = root.url()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := root.Get()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected OK, got %d", res.StatusCode)
+	}
+
+	if res.Request.URL.String() != ts.URL+"/" {
+		t.Errorf("Expected url to be %s, got %s", ts.URL+"/", res.Request.URL)
+	}
+
+	if hits["/"] != 4 {
+		t.Errorf("Expected 4 request to /, got %d", hits["/"])
 	}
 }
 
